c0d870aec07f26c8b8d3d9051686650defb63408
Isaiah peter <peterisaiah4fun@gmail.com>
2022-10-12 00:38:01 +0100

    please work

--- setup_mysql_test.sql
+++ setup_mysql_test.sql
@@ -1,3 +1,5 @@
+-- create user and database
+-- grand user privileges
 CREATE DATABASE IF NOT EXISTS `hbnb_test_db`;
 CREATE USER IF not EXISTS 'hbnb_test'@'localhost' IDENTIFIED BY 'hbnb_test_pwd';
 GRANT ALL PRIVILEGES ON hbnb_test_db.* TO 'hbnb_test'@'localhost';
