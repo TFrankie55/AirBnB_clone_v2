c8dbae0741369a8fb89ad7a1dba43e01e5a6ca51
Isaiah peter <peterisaiah4fun@gmail.com>
2022-10-12 19:55:41 +0100

    work plase

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -23,6 +23,7 @@ class DBStorage:
     __session: None
 
     def __init__(self):
+        """Returns a dictionary of models currently in storage"""
         user = os.getenv("HBNB_MYSQL_USER")
         password = os.getenv("HBNB_MYSQL_PWD")
         host = os.getenv("HBNB_MYSQL_HOST")
@@ -34,6 +35,7 @@ class DBStorage:
             Base.metadata.drop_all(self.__engine)
 
     def all(self, cls=None):
+        """Returns a dictionary of models currently in storage"""
         if cls != None:
             rows = self.__session.query(cls).all()
             return {"{}.{}".format(type(obj).__name__, obj.id): obj for obj in
@@ -47,17 +49,21 @@ class DBStorage:
                 objects}
 
     def new(self, obj):
+        """Adds new object to storage dictionary"""
         print(obj)
         self.__session.add(obj)
 
     def save(self):
+        """save new object to db"""
         self.__session.commit()
 
     def delete(self, obj=None):
+        """remove new object from session"""
         if obj != None:
             self.__session.delete(obj)
 
     def reload(self):
+        """Create the table"""
         Base.metadata.create_all(self.__engine)
         self.__session = sessionmaker(
             bind=self.__engine, expire_on_commit=False)
