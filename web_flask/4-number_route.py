c0e64fdb443f0204265cde1b78a0c84016e8f785
Tosin Owoeye <101506432+TFrankie55@users.noreply.github.com>
2022-11-15 03:36:51 +0100

    number_route

--- web_flask/4-number_route.py
+++ web_flask/4-number_route.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
-""" Starts a simple Flask web app.
+""" A script that starts a simple Flask web app.
     The app listens on 0.0.0.0, port 5000.
     Routes:
         /: Displays 'Hello HBNB!'
         /hbnb: Displays 'HBNB'
         /c/<text>: Displays 'C <text>'
         /python/(<text>): Displays 'Python <text>'
+        /number/<n>: display 'n is a number' if n is an integer
 """
 from flask import Flask
 
@@ -19,27 +20,27 @@ def hello():
 
 
 @app.route('/hbnb', strict_slashes=False)
-def hello_hbnb():
+def hbnb():
     """Displays 'HBNB'"""
     return "HBNB"
 
 
 @app.route('/c/<text>', strict_slashes=False)
-def hello_c(text, strict_slashes=False):
+def c_text():
     """ Displays 'C ' followed by <text>"""
     return "C {}".format(text.replace('_', ' '))
 
 
 @app.route("/python", strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
-def hello_python(text='is cool'):
+def python_text(text='is cool'):
     """ Displays 'Python ' followed by <text>"""
     return "Python {}".format(text.replace('_', ' '))
 
 
-@app.route('/number/<n>', strict_slashes=False)
+@app.route('/number/<int:n>', strict_slashes=False)
 def number_int(n):
-    """Displays 'n is a number' only if n is an int"""
+    """ Displays 'n is a number' only if n is an integer"""
     return "{:d} is a number".format(n)
 
 
